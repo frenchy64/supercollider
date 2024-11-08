d80c45be5a09ec33c426eb474d8062e45ce14ca4
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-11-08 17:15:11 -0600

    [skip ci]

--- actions/build_params.clj
+++ actions/build_params.clj
@@ -1,33 +1,33 @@
 #!/usr/bin/env bb
 ;; output build params to "params"
 
-(ns build-params)
-
-(defn linux-matrix []
-  (-> []
-      (into (map #(into {:os-version (if (<= % 12) "22.04" "24.04")
-                         :c-compiler (str "gcc-" %)
-                         :cxx-compiler (str "g++-" %)}
-                        (case %
-                          9 {:run-tests true}
-                          13 {:job-name "use system libraries"
-                              :use-syslibs true}
-                          14 {:job-name "shared libscsynth"
-                              :shared-libscsynth true}
-                          {})))
-        [9 11 12 13 14])
-      (into (map #(do {:os-version (if (<= % 11) "22.04" "24.04")
-                       :c-compiler (str "clang-" %)
-                       :cxx-compiler (str "clang++-" %)}))
-            [11 15 16 17 18])))
+(ns build-params
+  (:require [cheshire.core :as json]))
 
 (defn all-params []
-  {:sc-version (or (System/getenv "SC_VERSION")
-                   (throw (ex-info "Must set $SC_VERSION" {})))
-   :linux-matrix (linux-matrix)
+  {:sc-version (or (System/getenv "SC_VERSION") (throw (ex-info "Must set $SC_VERSION" {})))
+   :linux-matrix (-> []
+                     (into (map #(into {:os-version (if (<= % 12) "22.04" "24.04")
+                                        :c-compiler (str "gcc-" %)
+                                        :cxx-compiler (str "g++-" %)}
+                                       (case %
+                                         12 {:run-tests true}
+                                         13 {:job-name "use system libraries"
+                                             :use-syslibs true}
+                                         14 {:job-name "shared libscsynth"
+                                             :shared-libscsynth true}
+                                         {})))
+                           [9 11 12 13 14])
+                     (into (map #(do {:os-version (if (<= % 11) "22.04" "24.04")
+                                      :c-compiler (str "clang-" %)
+                                      :cxx-compiler (str "clang++-" %)}))
+                           [11 15 16 17 18]))
    :macos-matrix []})
 
-(defn -main [])
+(defn -main []
+  (println "Setting build params:")
+  (println (json/encode (all-params) {:pretty true}))
+  (println "echo" (pr-str (str "params=" (json/encode (all-params)))) ">> $GITHUB_OUTPUT"))
 
 (when (= *file* (System/getProperty "babashka.file"))
   (apply -main *command-line-args*))
